dffc888c7871ee69738750d14560fdedb20bdc80
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-10 15:29:27 -0500

    Randomly selected quote

--- dynamic/custom.js
+++ dynamic/custom.js
@@ -3,9 +3,16 @@ $(document).ready(function() {
         $.getJSON("/dynamic/json/quotes.json", function(json) {
             var quoteText = "";
             var attributeText = "";
+            var totalQuotes = json.length;
+
+            function randomQuote() {
+                return Math.floor(Math.random() * (totalQuotes));
+            }
+            var chosenQuote = randomQuote();
+            console.log(chosenQuote);
 
             json = json.filter(function(val) {
-                return (val.id == Math.floor(Math.random() * (json.length - 1)));
+                return (val.id == chosenQuote);
             });
 
             json.forEach(function(val) {
