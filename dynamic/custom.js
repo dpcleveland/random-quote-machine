1ff2eccdf74cf7743a5d2dfce57f1915c90ca625
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-11 20:41:14 -0500

    Swap out background colors for each quote

--- dynamic/custom.js
+++ dynamic/custom.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
             var quoteText = "";
             var attributeText = "";
             var totalQuotes = json.length;
+            var newBackgroundColor = "";
 
             function randomQuote() {
                 return Math.floor(Math.random() * (totalQuotes));
@@ -19,10 +20,14 @@ $(document).ready(function() {
                 quoteText += "<i class='fa fa-quote-left' aria-hidden='true'></i> ";
                 quoteText += val.quote;
                 attributeText += val.attribute;
+                newBackgroundColor = val.color;
             });
 
             $("#quote-text").html(quoteText);
             $("#attribution-text").html(attributeText);
+            $('body').css('background-color', newBackgroundColor);
+            $('.actions button').css('background-color', newBackgroundColor);
+            $('.social ul li').css('background', newBackgroundColor);
         });
     });
 });
